5f4ee7fa679c16cc737e7bf939fb4e5cdd9cd13e
Alex Elliott <alexjaelliott@gmail.com>
2025-12-04 21:36:16 -0800

    Added PythonRunner class to FormApp
    This class was developed earlier and can be used to run and manage a python file. This can be used in the future to accomplish speech to text for voice control.

---
+++ formApp/formApp/PythonRunner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PythonTest
+{
+    public class PythonRunner
+    {
+        private Process _process;
+
+        public PythonRunner(String script) 
+        { 
+            _process = new Process();
+            _process.StartInfo.FileName = "py";
+            _process.StartInfo.Arguments = script;
+            _process.StartInfo.UseShellExecute = false;
+            _process.StartInfo.RedirectStandardInput = true;
+            _process.StartInfo.RedirectStandardOutput = true;
+            _process.StartInfo.CreateNoWindow = true;
+        }
+
+        public void Start(DataReceivedEventHandler callback)
+        {
+            _process.OutputDataReceived += callback;
+
+            _process.Start();
+            _process.BeginOutputReadLine();
+        }
+
+        public void Send(string msg)
+        {
+            _process.StandardInput.WriteLine(msg);
+            _process.StandardInput.Flush();
+        }
+    }
+
+}
