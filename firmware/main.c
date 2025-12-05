77890e18c660152d5817e0fd156f797b079ce41a
Alex Elliott <alexjaelliott@gmail.com>
2025-12-04 21:25:37 -0800

    Setup empty firmware project

---
+++ firmware/main.c
@@ -0,0 +1,15 @@
+/*
+ * Firmware package for the Automated Spice Rack Project
+ * completed in accordance with UBC Mech 423 2025/26
+ * 
+ * Austin Chuong - 24854184 - 
+ * Alex Elliott  - 99567398 - alexjaelliott@gmail.com
+ */
+
+#include "driverlib.h"
+#include <msp430.h>
+#include <stdio.h>
+
+void main (void)
+{
+}
