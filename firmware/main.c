f77efca1af45c349ae19559c761e1f6ef9be139c
Alex Elliott <alexjaelliott@gmail.com>
2026-01-14 14:57:07 -0800

    Added stepper driver code

--- firmware/main.c
+++ firmware/main.c
@@ -28,21 +28,27 @@ volatile Buffer taskBuffer = {{}, 0, 0, 0};
 
 volatile int started = 0;
 volatile int state = 0;
+volatile unsigned char activeIndex = 0;
 
 volatile bool spiceSeen = false;
+
 volatile long encTicks = 0;
 volatile long desTicks = 0;
-volatile unsigned int stepsRem = 0;
-volatile unsigned char stepperDir = 0;
 
-volatile unsigned char activeIndex;
+volatile unsigned int PWM_25;
+volatile unsigned int stepsRem = 0;
+volatile signed int stepperState = 0;
+volatile signed int stepperDir = 0;
+//volatile unsigned int stepperSpeed = (8000000 / 5000) - 1;
 
 // ====================================================================== Prototypes
 
 void uartTransmit(int byte);
 
-void setMotorSpeedDirection(int value);
-void setMotorDistance(int dist);
+void setMotorSpeedDirection(int value); // DC Motor
+
+unsigned int stepLookup(int state); // Stepper Motor
+void stepMotor(void);
 
 void bufferPush(volatile Buffer *b, int byte);
 int bufferPop(volatile Buffer *b);
@@ -59,11 +65,19 @@ void uartTransmit(int byte) {
 }
 
 void setMotorSpeedDirection(int value) {
+    if (value > 100) {
+        value = 100;
+    } else if (value < 0) {
+        value = 0;
+    }
+    
     // Speed
     TB0CCR2 = TB0CCR0 * abs(value) / 100;
 
     // Direction
-    if (value > 0) {
+    if (value == 0) {
+        P3OUT |= BIT6 + BIT7; 
+    } else if (value > 0) {
         P3OUT |= BIT6;
         P3OUT &= ~BIT7;
     } else {
@@ -72,8 +86,37 @@ void setMotorSpeedDirection(int value) {
     }
 }
 
-void setMotorDistance(int dist) {
+unsigned int stepLookup(int state) {
+    int table[] = {
+        0b1001,
+        0b1000,
+        0b1100,
+        0b0100,
+        0b0110,
+        0b0010,
+        0b0011,
+        0b0001,
+    };
+    return table[state];
+}
 
+void stepMotor(void) {
+    // lookup the right signals to each stepper wire
+    unsigned int value = stepLookup(stepperState);
+    uint8_t bit3 = (value >> 3) & 1;
+    uint8_t bit2 = (value >> 2) & 1;
+    uint8_t bit1 = (value >> 1) & 1;
+    uint8_t bit0 = (value >> 0) & 1;
+    
+    // output either the logic high or logic low
+    if (bit3) PJOUT |= BIT0; else PJOUT &= ~BIT0;
+    if (bit2) PJOUT |= BIT1; else PJOUT &= ~BIT1;
+    if (bit1) PJOUT |= BIT2; else PJOUT &= ~BIT2;
+    if (bit0) PJOUT |= BIT3; else PJOUT &= ~BIT3;
+
+    stepperState = (stepperState + stepperDir) & 0x07; // increments state and resets to 0 after 7
+    if (stepperState < 0) 
+        stepperState = 7;
 }
 
 void bufferPush(volatile Buffer *b, int byte) {
@@ -151,18 +194,23 @@ void init(void) {
     // ------------------------------------------------------------------ Timer B
 
     // Motor PWM Timer
-    P1DIR |= BIT5;
-    P1SEL0 |= BIT5;
-    P1SEL1 &= ~BIT5;
+    P1DIR |= BIT4 | BIT5;
+    P1SEL0 |= BIT4 | BIT5;
+    P1SEL1 &= ~(BIT4 | BIT5);
 
     TB0CTL = TBSSEL__SMCLK | MC__UP | TBCLR; 
     TB0CCR0 = 160 - 1;   // 50 kHz PWM
+    TB0CCTL1 = OUTMOD_7;
     TB0CCTL2 = OUTMOD_7;
-    TB0CCR2 = 50 * TB0CCR0 / 100;
+    TB0CCR1 = 60 * TB0CCR0 / 100; // Const. for Stepper Moter
+    TB0CCR2 = 50 * TB0CCR0 / 100; // Variable for DC Motor
+
+    // set PJ.0-3 to outputs for the offboard motor driver
+    PJDIR |= BIT0 | BIT1 | BIT2 | BIT3;
 
     // Encoder Reporting Timer
     TB1CTL = TBSSEL__SMCLK | ID__8 | MC__STOP | TBCLR; 
-    TB1CCR0 = 50000 - 1;   // 20 Hz PWM
+    TB1CCR0 = 5000 - 1;   // 200 Hz PWM
     TB1CCTL0 = CCIE; 
 
     // ------------------------------------------------------------------ UART1
@@ -174,12 +222,12 @@ void init(void) {
     UCA1CTLW0 |= UCSSEL__SMCLK; // Select SMCLK as clock source
 
     // 9600 Baud Rate
-    // UCA1BRW = 52;                                 // INT(833 / 16) = 52
-    // UCA1MCTLW = UCOS16 | UCBRF_1 | (0x49 << 8);   // Oversampling, fractional part
+    UCA1BRW = 52;                                 // INT(833 / 16) = 52
+    UCA1MCTLW = UCOS16 | UCBRF_1 | (0x49 << 8);   // Oversampling, fractional part
 
     // 38400 Baud Rate
-    UCA1BRW = 13;                                 // INT(833 / 16) = 52
-    UCA1MCTLW = UCOS16 | UCBRF_0 | (0x84 << 8);   // Oversampling, fractional part
+    // UCA1BRW = 13;                                 // INT(833 / 16) = 52
+    // UCA1MCTLW = UCOS16 | UCBRF_0 | (0x84 << 8);   // Oversampling, fractional part
 
     UCA1CTLW0 &= ~UCSWRST;  // Initialize eUSCI
     UCA1IE |= UCRXIE;   // Enable USCI_A0 RX interrupt
@@ -223,7 +271,7 @@ void stateMachine(void) {
             if (abs(desTicks - encTicks) < 5) { state = 3; }
             break;
         case 3: // Give Spice - Step 3 - Extend the pusher arm
-            stepperDir = 0;
+            stepperDir = -1;
             stepsRem = 1000; // TODO replace with real values
             state = 4;
             break;
@@ -236,6 +284,7 @@ void stateMachine(void) {
         case 6: // Give Spice - Step 6 - Retract the pusher arm
             stepperDir = 1;
             stepsRem = 1000;
+            state = 7;
             break;
         case 7: // Give Spice - Step 7 - Wait until arm is ractracted (stepper steps is completed)
             if (stepsRem < 1) { state = 8; }
@@ -268,7 +317,7 @@ void stateMachine(void) {
         case 18: // Return Spice - Step 8 - Continue onto next task
             state = 0;
             break;
-        default: state = 0; // Handle brocken edge cases
+        default: state = 0; // Handle broken edge cases
     }
 }
 
@@ -310,11 +359,15 @@ __interrupt void Port_1_ISR(void) {
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void TIMER1_B0_ISR(void) {
     encTicks = encTicks + TA0R - TA1R;
-
     TA0R = 0;
     TA1R = 0;
 
-    // should PID be updated here as well?
+    if (stepsRem > 0) {
+        stepMotor();
+        stepsRem--;
+    }
+
+    setMotorSpeedDirection(0.5 * (desTicks - encTicks)); // todo replace Kp
 }
 
 #pragma vector = USCI_A1_VECTOR
@@ -328,4 +381,3 @@ __interrupt void USCI_A1_ISR(void) {
     RxByte = UCA1RXBUF; // Get the new byte from the Rx buffer
     bufferPush(&rxBuffer, RxByte);
 }
-
